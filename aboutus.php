e06d94883bc9a4761985dcd17e3e9cd049bf5ada
rizvi <k214578@nu.edu.pk>
2024-02-24 16:03:35 +0500

    loading php files

---
+++ aboutus.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>SafarNama | About Us</title>
+<link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
+<link href="stylecss.css" rel='stylesheet' type='text/css'/>
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css'/>
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!--js--> 
+<script src="js/jquery.min.js"></script>
+
+<!--/js-->
+<!--animated-css-->
+<link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
+<script src="js/wow.min.js"></script>
+<script>
+ new WOW().init();
+</script>
+</head>
+
+<body>
+<?php include('function.php'); ?>
+<?php include('top.php'); ?>
+<!--/sticky-->
+<?php include('slider.php'); ?>
+<div style="height:50px"></div>
+
+
+
+
+<div style="width:900px; padding-left:100px">
+<table cellpadding="0px" cellspacing="0" width="900px">
+<tr><td class="headingText">About Us</td></tr>
+<tr><td class="paraText" width="900px">
+<b>About SafarNama</b></br>
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut tortor pretium viverra suspendisse potenti. Vehicula ipsum a arcu cursus vitae congue mauris. Quam pellentesque nec nam aliquam. Justo donec enim diam vulputate. Aliquam malesuada bibendum arcu vitae. Pulvinar pellentesque habitant morbi tristique. Non enim praesent elementum facilisis leo. Vel risus commodo viverra maecenas accumsan lacus vel. Ultricies leo integer malesuada nunc vel risus commodo viverra. Nunc id cursus metus aliquam eleifend mi in. Pulvinar proin gravida hendrerit lectus a. Elementum facilisis leo vel fringilla est ullamcorper eget nulla. Consectetur a erat nam at lectus urna duis convallis convallis. Tempus iaculis urna id volutpat lacus laoreet non curabitur. Sem nulla pharetra diam sit amet nisl suscipit adipiscing bibendum. Vitae proin sagittis nisl rhoncus mattis rhoncus urna neque.
+
+Odio pellentesque diam volutpat commodo sed egestas egestas. Aliquam eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis. Fringilla ut morbi tincidunt augue interdum velit euismod in pellentesque. Proin fermentum leo vel orci porta non. Dignissim enim sit amet venenatis urna. Arcu bibendum at varius vel pharetra vel. Sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper. Morbi blandit cursus risus at ultrices. Lobortis feugiat vivamus at augue eget arcu dictum varius duis. Amet dictum sit amet justo donec. At quis risus sed vulputate odio ut enim blandit.
+
+</td></tr>
+<tr><td colspan="3"><img src="images/ttttt.jpg" width="1200px" height="700px"/></td></tr>
+
+<tr><td colspan="3" class="paraText"><div class="container">		  
+		 <div class="about-grids">
+			 <h4><b><span class="icon1"></span>Our Vision</b></h4>
+             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut tortor pretium viverra suspendisse potenti. Vehicula ipsum a arcu cursus vitae congue mauris. Quam pellentesque nec nam aliquam. Justo donec enim diam vulputate. Aliquam malesuada bibendum arcu vitae. Pulvinar pellentesque habitant morbi tristique. Non enim praesent elementum facilisis leo. Vel risus commodo viverra maecenas accumsan lacus vel. Ultricies leo integer malesuada nunc vel risus commodo viverra. Nunc id cursus metus aliquam eleifend mi in. Pulvinar proin gravida hendrerit lectus a. Elementum facilisis leo vel fringilla est ullamcorper eget nulla. Consectetur a erat nam at lectus urna duis convallis convallis. Tempus iaculis urna id volutpat lacus laoreet non curabitur. Sem nulla pharetra diam sit amet nisl suscipit adipiscing bibendum. Vitae proin sagittis nisl rhoncus mattis rhoncus urna neque.
+
+Odio pellentesque diam volutpat commodo sed egestas egestas. Aliquam eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis. Fringilla ut morbi tincidunt augue interdum velit euismod in pellentesque. Proin fermentum leo vel orci porta non. Dignissim enim sit amet venenatis urna. Arcu bibendum at varius vel pharetra vel. Sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper. Morbi blandit cursus risus at ultrices. Lobortis feugiat vivamus at augue eget arcu dictum varius duis. Amet dictum sit amet justo donec. At quis risus sed vulputate odio ut enim blandit.
+Tellus molestie nunc non blandit massa enim nec dui nunc. Placerat orci nulla pellentesque dignissim enim sit. Nulla pellentesque dignissim enim sit amet venenatis urna. Viverra justo nec ultrices dui sapien. Donec adipiscing tristique risus nec feugiat in. Eu tincidunt tortor aliquam nulla facilisi cras fermentum odio. Fames ac turpis egestas integer eget aliquet nibh.
+		 </div></td></tr>
+		<tr><td class="paraText"> <div class="about-grids grid2">
+			 <h4><b><span class="icon2"></span>Our Mission</b></h4>
+             Tellus molestie nunc non blandit massa enim nec dui nunc. Placerat orci nulla pellentesque dignissim enim sit. Nulla pellentesque dignissim enim sit amet venenatis urna. Viverra justo nec ultrices dui sapien. Donec adipiscing tristique risus nec feugiat in. Eu tincidunt tortor aliquam nulla facilisi cras fermentum odio. Fames ac turpis egestas integer eget aliquet nibh.
+
+ </div></td></tr>
+		<tr> <td class="paraText"><div class="about-grids">
+			 <h4><b><span class="icon3"></span>Safety Information</b></h4>
+             Tellus molestie nunc non blandit massa enim nec dui nunc. Placerat orci nulla pellentesque dignissim enim sit. Nulla pellentesque dignissim enim sit amet venenatis urna. Viverra justo nec ultrices dui sapien.
+ some safety tips are:<br/><br/>
+<b>TRANSPORTATION SAFETY TIPS:</b><br/>
+Tellus molestie nunc non blandit massa enim nec dui nunc. Placerat orci nulla pellentesque dignissim enim sit. Nulla pellentesque dignissim enim sit amet venenatis urna. Viverra justo nec ultrices dui sapien.<br/>
+Be mindful of other passengers traveling with you and never hesitate to report any suspicious act to the concerned authorities.<br/>
+ Never share cab or taxi with strangers. As carjacking is the biggest threat for a traveler.<br/>
+  While traveling by flight, always pay attention to the preflight briefing about closest emergency exit door, fastening your seat belts properly, applying oxygen masks and other important details to be followed in case of emergency.<br/>
+    Do not ignore all significant details provided by flight attendant. They are prime source of aid in case of emergency and unexpected turbulence.<br/>
+    Wear money belts, hidden neck and waist pouches, hidden pocket wallets and zippered compartments under your clothes to avoid pick pocketing.<br/><br/>
+
+<b>HOTEL SAFETY TIPS: </b><br/><br/>
+
+Choosing the safest accommodation at an unknown place could be a biggest concern for you. Therefore be aware of certain tips while choosing a safest hotel for your self.<br/>
+
+    Never hesitate to choose the best and safest hotel in town.<br/>
+    Always carry all the important documents with you while going out of your hotel room.<br/>
+    Double check window and door locks before leaving the hotel room.<br/>
+    Prefer to keep your important luggage at receptions locker.<br/>
+    Get well versed with emergency exits, fire escapes, emergency stairwells to be used in case of emergency.<br/>
+     To make most out of your trip these basic general safety tips can prove to be very helpful.
+		 </div>
+	 </div>
+    </td> </tr></table>
+
+</div>
+
+
+
+<div style="clear:both"></div>
+
+<?php include('bottom.php'); ?>
+</body>
+</html>
--- admin/deleteuser.php
+++ admin/deleteuser.php
@@ -1,5 +1,4 @@
 <?php if(!isset($_SESSION)) { session_start(); } ?>
-
 <!--
 Author: W3layouts
 Author URL: http://w3layouts.com
@@ -11,9 +10,16 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <head>
 <title>My-tour bootstrap Design website | Home :: w3layouts</title>
 <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
+
+<link href="style.css" rel="stylesheet" type="text/css" />
+
 <link href="../css/bootstrap.css" rel='stylesheet' type='text/css'/>
 <link href="../css/style.css" rel="stylesheet" type="text/css" media="all"/>
 <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+
+
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
 <!--js--> 
 <script src="js/jquery.min.js"></script>
@@ -30,11 +36,22 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <body>
 <!--header-->
 <!--sticky-->
-
 <?php
 if($_SESSION['loginstatus']=="")
 {
 	header("location:loginform.php");
+}
+?>
+
+<?php include('function.php'); ?>
+<?php
+if(isset($_POST["sbmt"]))
+{
+	$cn=makeconnection();
+	$s="delete from users  where Username='" . $_POST["t1"] . "'";
+	mysqli_query($cn,$s);
+	mysqli_close($cn);
+	echo "<script>alert('Record Delete');</script>";
 }
 ?>
 
@@ -45,10 +62,68 @@ if($_SESSION['loginstatus']=="")
 <div class="col-sm-3" style="border-right:1px solid #999; min-height:450px;">
 <?php include('left.php'); ?>
 </div>
-<div class="col-sm-9" align="center"><img src="adminpics/ert.jpg" style="padding-top:40px"  width="500px" height="400px"/></div>
+<div class="col-sm-9">
+
+
+
+
+
+<form method="post">
+<table border="0" width="400px" height="200px" align="center" class="tableshadow">
+<tr><td colspan="2" class="toptd">Delete User</td></tr>
+<tr><td class="lefttxt">Select User</td><td><select name="t1" required/><option value="">Select</option>
+
+<?php
+$cn=makeconnection();
+$s="select * from users";
+$result=mysqli_query($cn,$s);
+$r=mysqli_num_rows($result);
+//echo $r;
+
+while($data=mysqli_fetch_array($result))
+{
+	
+		echo "<option value=$data[0]>$data[0]</option>";
+	
+}
+mysqli_close($cn);
+
+
+
+?>
+
+</select>
+
+<?php
+if(isset($_POST["show"]))
+{
+$cn=makeconnection();
+$s="select * from users where Username='" .$_POST["t1"] ."'";
+$result=mysqli_query($cn,$s);
+$r=mysqli_num_rows($result);
+//echo $r;
+
+$data=mysqli_fetch_array($result);
+$Username=$data[0];
+$Pass=$data[1];
+$Usertype=$data[2];
+
+mysqli_close($cn);
+
+}
+
+?>
+
+</td></tr>
+
+<tr><td>&nbsp;</td><td ><input type="submit" value="Delete" name="sbmt" /></td></tr>
+</table>
+</form>
+</div>
 
 
 </div>
 <?php include('bottom.php'); ?>
 </body>
 </html>
+
